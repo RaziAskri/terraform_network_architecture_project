484551ab14a17c1bc45aea7d37cbd9593e6fd34d
RaziAskri <65558680+RaziAskri@users.noreply.github.com>
2025-04-02 14:01:31 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,30 +1,25 @@
-#provider
+# Provider block
 provider "aws" {
-  #user infos and default region are already assigned in the aws cli
+  # user infos and default region are already assigned in the AWS CLI
 }
 
 ###############
 ######vpc######
 ###############
 
-#the existing vpc id hardcoded
-variable "vpc_id" {
-  default = "vpc-07e2e0f58f11c50d6"
-}
-
-#assign the vpc var to data block
+# assign the vpc var to data block
 data "aws_vpc" "terraform_razi_vpc" {
-  id = var.vpc_id
+  id = "vpc-07e2e0f58f11c50d6"
 }
 
 ###############
-#public subnet#
+# Public subnet#
 ###############
 resource "aws_subnet" "terraform_razi_public_subnet" {
-  vpc_id     = data.aws_vpc.terraform_razi_vpc.id
-  availability_zone       = "eu-west-1b"    # Specify Availability Zone B
-  #cidr_block = cidrsubnet(data.aws_vpc.terraform_razi_vpc.cidr_block, 8, 1)
-  cidr_block = "50.30.26.0/24"
+  vpc_id            = data.aws_vpc.terraform_razi_vpc.id
+  availability_zone = "eu-west-1b" # Specify Availability Zone B
+  cidr_block        = "50.30.26.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "terraform_razi_public_subnet"
@@ -32,42 +27,38 @@ resource "aws_subnet" "terraform_razi_public_subnet" {
 }
 
 ################
-#private subnet#
+# Private subnet#
 ################
 resource "aws_subnet" "terraform_razi_private_subnet" {
-  vpc_id     = data.aws_vpc.terraform_razi_vpc.id
-  availability_zone       = "eu-west-1b"    # Specify Availability Zone B
-  #cidr_block = cidrsubnet(data.aws_vpc.terraform_razi_vpc.cidr_block, 8, 2)
-  cidr_block = "50.30.27.0/24"
+  vpc_id            = data.aws_vpc.terraform_razi_vpc.id
+  availability_zone = "eu-west-1b" # Specify Availability Zone B
+  cidr_block        = "50.30.27.0/24"
 
   tags = {
     Name = "terraform_razi_private_subnet"
   }
 }
 
-##################
-#internet gateway#
-##################
-
+###################
+# Internet gateway#
+###################
 data "aws_internet_gateway" "terraform_razi_existing_internet_gateway" {
   filter {
     name   = "tag:Name"
-    values = ["km-abs-gateway-tp-aws"]  # the Name of the existing internet gateway
+    values = ["km-abs-gateway-tp-aws"] # the Name of the existing internet gateway
   }
 }
 
-##############
-##elastic ip##
-##############
-
-#fetch the existing eip using data blocks
+###############
+## Elastic IP##
+###############
 data "aws_eip" "terraform_razi_existing_eip" {
-  id = "eipalloc-0dab1c043a0c70f9d" #the existing eip allocation id
+  id = "eipalloc-040ed811f144399f8" # the existing eip allocation id
 }
 
-#############
-#Nat gateway#
-#############
+##############
+# Nat gateway#
+##############
 resource "aws_nat_gateway" "terraform_razi_Nat_Gateway" {
   allocation_id = data.aws_eip.terraform_razi_existing_eip.id
   subnet_id     = aws_subnet.terraform_razi_public_subnet.id
@@ -78,9 +69,9 @@ resource "aws_nat_gateway" "terraform_razi_Nat_Gateway" {
 }
 
 #############################################
-#public route table for the internet gateway#
+# Create a Route Table for the Public Subnet#
 #############################################
-resource "aws_route_table" "terraform_razi_route_table_for_internet_gw" {
+resource "aws_route_table" "terraform_razi_public_route_table" {
   vpc_id = data.aws_vpc.terraform_razi_vpc.id
 
   route {
@@ -89,23 +80,25 @@ resource "aws_route_table" "terraform_razi_route_table_for_internet_gw" {
   }
 
   tags = {
-    Name = "terraform_razi_route_table_for_internet_gw"
+    Name = "terraform_razi_public_route_table"
   }
 }
 
-#######################################################################################
-#this is the association resource that associates the route table to the public subnet#
-#######################################################################################
-resource "aws_route_table_association" "terraform_route_table_association_to_internet_gw" {
+##################################################
+# Associate the Route Table to the Public Subnet #
+##################################################
+resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.terraform_razi_public_subnet.id
-  route_table_id = aws_route_table.terraform_razi_route_table_for_internet_gw.id
+  route_table_id = aws_route_table.terraform_razi_public_route_table.id
 }
 
 
-########################################
-#public route table for the NAT gateway#
-########################################
-resource "aws_route_table" "terraform_razi_route_table_for_NAT_gateway" {
+###############################################
+# Create a Route Table for the private Subnet #
+###############################################
+
+#create a private route table
+resource "aws_route_table" "terraform_razi_private_route_table" {
   vpc_id = data.aws_vpc.terraform_razi_vpc.id
 
   route {
@@ -114,80 +107,54 @@ resource "aws_route_table" "terraform_razi_route_table_for_NAT_gateway" {
   }
 
   tags = {
-    Name = "terraform_razi_route_table_for_NAT_gateway"
+    Name = "terraform_razi_private_route_table"
   }
 }
 
-########################################################################################
-#this is the association resource that associates the route table to the private subnet#
-########################################################################################
-resource "aws_route_table_association" "terraform_route_table_association_to_NAT_gw" {
-  subnet_id      = aws_subnet.terraform_razi_private_subnet.id
-  route_table_id = aws_route_table.terraform_razi_route_table_for_NAT_gateway.id
+###################################################
+# Associate the Route Table to the private Subnet #
+###################################################
+
+# Associate the private route table with the private subnet
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.terraform_razi_private_subnet.id # Reference to the private subnet
+  route_table_id = aws_route_table.terraform_razi_private_route_table.id
 }
 
-####################################
-#public EC2 key & instance creation#
-####################################
+#####################################
+# Public EC2 instance key & creation#
+#####################################
 
-# Generate a new private key for EC2 instance
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-# Save the private key locally
-resource "local_file" "private_key" {
-  content  = tls_private_key.pk.private_key_pem
-  filename = "C:/Users/RaziASKRI/Desktop/projects/aws_instances_keys/ec2_private_key_public_ec2.pem"  # Path to save the private key
-}
-
-
-# Create an EC2 Key Pair using the generated private key
-resource "aws_key_pair" "terraform_razi_ec2_public_pk" {
-  key_name   = "ec2-public-key-pair"
-  public_key = tls_private_key.pk.public_key_openssh
-}
-
-
+# EC2 instance creation
 resource "aws_instance" "terraform_razi_ec2_public" {
   
-  ami             = "ami-0a007a006645e86ab"  # Replace with the AMI ID you want to use (e.g., Amazon Linux 2)
-  instance_type   = "t2.micro"                # Change instance type as needed
-  key_name        = aws_key_pair.terraform_razi_ec2_public_pk.key_name  # Use the key pair created above
-  subnet_id       = aws_subnet.terraform_razi_public_subnet.id #the subnet hosting the instance
-  associate_public_ip_address = true          # Automatically assign a public IP
-  security_groups = [aws_security_group.terraform_razi_ec2_sg.id]        # security group name or ID
+  ami                         = "ami-0a007a006645e86ab"                             # Replace with the AMI ID you want to use (e.g., Amazon Linux 2)
+  instance_type               = "t2.micro"                                          # Change instance type as needed
+  key_name                    = "terraform_razi_ec2_pk" 
+  subnet_id                   = aws_subnet.terraform_razi_public_subnet.id          # The subnet hosting the instance
+  associate_public_ip_address = true                                                # Automatically assign a public IP
+  security_groups             = [aws_security_group.terraform_razi_ec2_sg.id]       # Security group name or ID
 
   tags = {
     Name = "terraform_razi_ec2_public"
   }
 }
 
+
 #####################################
-#private EC2 key & instance creation#
+# Private EC2 instance key & creation#
 #####################################
 
-# Generate a new private key for EC2 instance
-resource "tls_private_key" "pk_private_ec2" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-# Create an EC2 Key Pair using the generated private key
-resource "aws_key_pair" "terraform_razi_ec2_private_pk" {
-  key_name   = "ec2-private-key-pair"
-  public_key = tls_private_key.pk_private_ec2.public_key_openssh
-}
 
 resource "aws_instance" "terraform_razi_ec2_private" {
 
-  ami             = "ami-0a007a006645e86ab"  # Replace with the AMI ID you want to use (e.g., Amazon Linux 2)
-  instance_type   = "t2.micro"                # Change instance type as needed
-  key_name        = aws_key_pair.terraform_razi_ec2_private_pk.key_name  # Use the key pair created above
-  subnet_id       = aws_subnet.terraform_razi_private_subnet.id #the subnet hosting the instance
-  associate_public_ip_address = false          # don't assign a public IP
-  security_groups = [aws_security_group.terraform_razi_ec2_sg.id]        # security group name or ID
+  ami                         = "ami-0a007a006645e86ab"                             # Replace with the AMI ID you want to use (e.g., Amazon Linux 2)
+  instance_type               = "t2.micro"                                          # Change instance type as needed
+  key_name                    = "terraform_razi_ec2_pk" 
+  subnet_id                   = aws_subnet.terraform_razi_private_subnet.id         # The subnet hosting the instance
+  associate_public_ip_address = false                                               # Don't assign a public IP
+  security_groups = [aws_security_group.terraform_razi_ec2_sg.id,
+  aws_security_group.terraform_razi_private_ec2_sg.id] # Security group name or ID
 
   tags = {
     Name = "terraform_razi_ec2_private"
@@ -195,11 +162,11 @@ resource "aws_instance" "terraform_razi_ec2_private" {
 }
 
 ##################################################
-#security group for ec2 instances & ingress rules#
+#security group for ec2 instances & sg rules#
 ##################################################
 
 resource "aws_security_group" "terraform_razi_ec2_sg" {
-  name        = "allow_https_ssh"
+  name        = "sg_for_public_and_private_ec2"
   description = "Allow HTTPS and SSH access"
   vpc_id      = data.aws_vpc.terraform_razi_vpc.id
 
@@ -210,20 +177,50 @@ resource "aws_security_group" "terraform_razi_ec2_sg" {
 
 # Règle pour autoriser l'accès SSH (port 22)
 resource "aws_security_group_rule" "allow_ssh" {
-  type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # Accès SSH depuis n'importe où (à restreindre si nécessaire)
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Accès SSH depuis n'importe où (à restreindre si nécessaire)
   security_group_id = aws_security_group.terraform_razi_ec2_sg.id
 }
 
 # Règle pour autoriser l'accès HTTPS (port 443)
 resource "aws_security_group_rule" "allow_https" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # Accès HTTPS depuis n'importe où (à restreindre si nécessaire)
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Accès HTTPS depuis n'importe où (à restreindre si nécessaire)
   security_group_id = aws_security_group.terraform_razi_ec2_sg.id
+}
+
+  # Outbound Rules (Allowing ICMP - ping - to anywhere)
+  resource "aws_security_group_rule" "outbound_rule" {
+    type        = "egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.terraform_razi_ec2_sg.id
+  }
+#####################################
+
+resource "aws_security_group" "terraform_razi_private_ec2_sg" {
+  name        = "sg_for_private_ec2"
+  description = "Allow SSH access from public EC2"
+  vpc_id      = data.aws_vpc.terraform_razi_vpc.id
+
+  tags = {
+    Name = "terraform_razi_private_ec2_sg"
+  }
+}
+
+resource "aws_security_group_rule" "allow_ssh_from_public_ec2" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.terraform_razi_private_ec2_sg.id
+  source_security_group_id = aws_security_group.terraform_razi_ec2_sg.id # Allow from public EC2's SG
 }
